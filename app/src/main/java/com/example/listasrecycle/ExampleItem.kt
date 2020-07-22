9c17e86326920a3590d79d664a3fee8add5b053c
Erik <erikciqueira.silva@gmail.com>
2020-07-22 17:35:14 -0300

    projeto lista de carros.

---
+++ app/src/main/java/com/example/listasrecycle/ExampleItem.kt
@@ -0,0 +1,3 @@
+package com.example.listasrecycle
+
+ data class ExampleItem(val imageResource: int, val text1: String, val text2: String )
---
+++ app/src/main/java/com/example/listasrecycle/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.listasrecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun generateDummyList(size: Int): List<ExampleItem> {
+        val list = ArrayList<ExampleItem>()
+        for (i in 0 until size) {
+            val drawable = when (i % 3) {
+                0 -> R.drawable.ic_android
+                1 -> R.drawable.ic_audio
+                else -> R.drawable.ic_sun
+            }
+            val item = ExampleItem(drawable, "Item $i", "Line 2")
+            list += item
+        }
+        return list
+    }
+}
